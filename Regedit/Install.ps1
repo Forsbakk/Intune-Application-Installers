2734ae04b16b4e3f457c10445608b88c227468a3
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-01-12 11:20:52 +0100

    HKLM,HKCR,HKCC import added

--- Regedit/Install.ps1
+++ Regedit/Install.ps1
@@ -1,1 +1,30 @@
-$payload = Get-Content "regedit\SamplePayload.json" | ConvertFrom-Json
+#
+#Install.ps1
+#Installs REG-files in Microsoft Intune (HKLM,HKCR,HKCC)
+#12.01.2018
+#JF;Horten kommune
+#
+$toAdd = (
+"@
+Windows Registry Editor Version 5.00
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CloudContent]
+`"DisableWindowsConsumerFeatures`"=dword:00000001
+@",
+"@
+Windows Registry Editor Version 5.00
+
+[HKEY_CLASSES_ROOT\WOW6432Node\CLSID\{679f85cb-0220-4080-b29b-5540cc05aab6}\ShellFolder]
+`"Attributes`"=dword:a0600000
+`"FolderValueFlags`"=dword:00000001
+
+[HKEY_CLASSES_ROOT\CLSID\{679f85cb-0220-4080-b29b-5540cc05aab6}\ShellFolder]
+`"Attributes`"=dword:a0600000
+`"FolderValueFlags`"=dword:00000001
+
+@"
+)
+ForEach ($reg in $toAdd) {
+    $reg | Out-File "$env:TEMP\temp.reg"
+    regedit.exe /s "$env:TEMP\temp.reg"
+}
