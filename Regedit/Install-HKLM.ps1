248337041722ec8f2ce67e759775d97a29a4ddbe
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-01-12 13:24:38 +0100

    change to hklm only

--- Regedit/Install-HKLM.ps1
+++ Regedit/Install-HKLM.ps1
@@ -1,6 +1,6 @@
 #
 #Install.ps1
-#Installs REG-files in Microsoft Intune (HKLM,HKCR,HKCC)
+#Installs REG-files in Microsoft Intune (HKLM)
 #12.01.2018
 #JF;Horten kommune
 #
@@ -10,19 +10,6 @@ $toAdd = (
 
 [HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CloudContent]
 `"DisableWindowsConsumerFeatures`"=dword:00000001
-
-",
-
-#Quick Access
-"Windows Registry Editor Version 5.00
-
-[HKEY_CLASSES_ROOT\WOW6432Node\CLSID\{679f85cb-0220-4080-b29b-5540cc05aab6}\ShellFolder]
-`"Attributes`"=dword:a0600000
-`"FolderValueFlags`"=dword:00000001
-
-[HKEY_CLASSES_ROOT\CLSID\{679f85cb-0220-4080-b29b-5540cc05aab6}\ShellFolder]
-`"Attributes`"=dword:a0600000
-`"FolderValueFlags`"=dword:00000001
 
 "
 )
