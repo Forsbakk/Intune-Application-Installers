809e2833721c64cd86eb8aab8604bd80657b14e8
subsequent <fanuelsen@mac.com>
2018-01-22 20:33:50 +0100

    Deploy-Teams

---
+++ MSI-Installer/Deploy-Teams.ps1
@@ -0,0 +1,53 @@
+#Install-Teams.ps1
+#Installs EXE applications in Microsoft Intune
+#11.01.2018
+#JF;Horten kommune
+#
+$AppName = "Microsoft Teams"
+$Installer = "Teams_windows_x64.exe"
+$InstArgs = "-s"
+$Uninstaller = $env:LOCALAPPDATA + "\Microsoft\Teams\Update.exe"
+$UninstArgs = "--uninstall -s"
+$wrkDir = "$PSscriptroot"
+$detection = ((Test-Path ($env:LOCALAPPDATA + "\Microsoft\Teams\Update.exe")) -and (!(Test-Path ($env:LOCALAPPDATA + "\Microsoft\Teams\.dead"))))
+$Mode = "Install" #Install or Uninstall
+
+#
+#INSTALL MODE
+#
+If ($mode -eq "Install") {
+    Write-Verbose "Starting installation script for $AppName"
+
+    #
+    #App detection
+    #
+    Write-Verbose "Detecting previous installations"
+
+    #
+    #Installation
+    #
+    If (!($detection)) {
+        Write-Verbose "$AppName is not detected, starting install"
+
+        Start-Process -FilePath $wrkDir\$Installer -ArgumentList $InstArgs -Wait
+    }
+
+    #
+    #Abort installation
+    #
+    Else {
+        Write-Verbose "$AppName detected, will NOT install"
+    }
+}
+
+#
+#UNINSTALL MODE
+#
+elseif ($mode -eq "Uninstall") {
+    If (Test-Path $Uninstaller) {
+        Start-Process $Uninstaller -ArgumentList $UninstArgs -Wait
+    }
+    Else {
+        Write-Verbose "Could not find uninstaller, aborting"
+    }
+}
---
+++ MSI-Installer/Start-Powershell.bat
@@ -0,0 +1,2 @@
+@Echo Off
+powershell.exe -executionpolicy unrestricted -file .\Deploy-Teams.ps1
