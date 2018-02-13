b85729241947e6b5bd30e464461e4ae288177866
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-02-13 19:27:34 +0100

    moar conf

--- Continuous delivery for Intune/Custom Execution/CustomExec.ps1
+++ Continuous delivery for Intune/Custom Execution/CustomExec.ps1
@@ -1,11 +1,19 @@
+$AdvInstConfig = $env:TEMP + "\AdvInstConfig.JSON"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/advinstall/Continuous%20delivery%20for%20Intune/Custom%20Execution/config.json" -OutFile $AdvInstConfig
+$AdvInstallers = Get-Content $AdvInstConfig | ConvertFrom-Json
+
 function Install-AdvancedApplication {
     Param (
         [string]$Name,
-        [string[]]$FilesToDwnload,
+        [psobject]$FilesToDwnload,
         [psobject]$Execution,
-        [string[]]$Detection
+        [psobject]$Detection,
+        [string]$wrkDir
     )
     foreach ($dwnload in $FilesToDwnload) {
+        $dwnload = $dwnload | Select-Object -ExpandProperty URL
         Invoke-WebRequest -Uri $dwnload -OutFile $wrkDir
     }
 }
+
+Install-AdvancedApplication -Name $AdvInstallers.Soultion.Name -FilesToDwnload $AdvInstallers.Soultion.FilesToDwnload
