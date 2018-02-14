2e0f1f07f8e3181fcb3383f42db44a40052fd05c
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-02-14 10:09:27 +0100

    complete

--- Continuous delivery for Intune/Custom Execution/Create-Config.ps1
+++ Continuous delivery for Intune/Custom Execution/Create-Config.ps1
@@ -1,35 +1,28 @@
 $CustomExec = @(
     @{
-        Soultion = @(
+        Name = "Deploy printer"
+        FilesToDwnload = @(
             @{
-                Name = "Deploy printer"
-                FilesToDwnload = @(
-                    @{
-                        FileName = "deploy-printer.ps1"
-                        URL = "http://sublog.org/storage/deploy-printer.ps1"
-                    },
-                    @{
-                        FileName = "printerdriver.zip"
-                        URL = "http://sublog.org/storage/printerdriver.zip"
-                    }
-                )
-                Execution = @(
-                    @{
-                        Execute = ""
-                        Arguments = ""
-                    }
-                )
-                Detection = @(
-                    @{
-                        Rule = "[bool](Get-WmiObject -Query `"select * from win32_printer where name like '%OneNote%'`")"
-                    },
-                    @{
-                        Rule = "[bool](Test-Path `"C:\tmp\test`")"
-                    }
-                )
-                wrkDir = "C:\Temp"                
+                FileName = "deploy-printer.ps1"
+                URL = "http://sublog.org/storage/deploy-printer.ps1"
+            },
+            @{
+                FileName = "printerdriver.zip"
+                URL = "http://sublog.org/storage/printerdriver.zip"
             }
         )
+        Execution = @(
+            @{
+                Execute = "powershell.exe"
+                Arguments = "-ExecutionPolicy Bypass -File `"C:\Windows\Temp\deploy-printer.ps1`""
+            }
+        )
+        Detection = @(
+            @{
+                Rule = "[bool](Get-WmiObject -Query `"select * from win32_printer where name like '%HK-ELEV%'`")"
+            }
+        )
+        wrkDir = "C:\Windows\Temp"                
     }
 )
-$CustomExec | ConvertTo-Json -Compress -Depth 4 | Out-File config.json
+$CustomExec | ConvertTo-Json -Compress | Out-File config.json
