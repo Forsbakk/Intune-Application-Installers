f3c959d5d50e7b280e8358740ca8843f0eead4b8
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-02-20 20:02:24 +0100

    hkcu live fix

--- Continuous delivery for Intune/HKCU/Create-Config.ps1
+++ Continuous delivery for Intune/HKCU/Create-Config.ps1
@@ -1,7 +1,7 @@
 $Downloads = @(
     @{
-        URL = "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/master/Continuous%20delivery%20for%20Intune/HKCU/regfiles/Test.reg"
-        detection = "[bool]((Get-ItemPropertyValue -Path REGISTRY::HKEY_USERS\.DEFAULT\SOFTWARE\Test -Name TestValue) -eq 0)"
+        URL = "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/master/Continuous%20delivery%20for%20Intune/HKCU/regfiles/ShownFileFmtPrompt.reg"
+        detection = "[bool]((Get-ItemPropertyValue -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Office\16.0\Common\General -Name ShownFileFmtPrompt) -eq 1)"
     }
 )
 $Downloads | ConvertTo-Json -Compress | Out-File config.json
