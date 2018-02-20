836e7cd2e98609d52b03059fc3ed7373fcfc79d1
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-02-20 18:29:53 +0100

    conf

--- Continuous delivery for Intune/HKCU/Create-Config.ps1
+++ Continuous delivery for Intune/HKCU/Create-Config.ps1
@@ -1,7 +1,7 @@
 $Downloads = @(
     @{
         URL = "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/HKCU/Continuous%20delivery%20for%20Intune/HKCU/regfiles/Test.reg"
-        detection = "[bool](if (Get-ItemPropertyValue -Path REGISTRY::HKEY_USERS\.DEFAULT\SOFTWARE\Test -Name TestValue) -eq 0)"
+        detection = "[bool]((Get-ItemPropertyValue -Path REGISTRY::HKEY_USERS\.DEFAULT\SOFTWARE\Test -Name TestValue) -eq 0)"
     }
 )
 $Downloads | ConvertTo-Json -Compress | Out-File config.json
