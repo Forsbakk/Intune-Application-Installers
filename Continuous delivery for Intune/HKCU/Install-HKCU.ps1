782e97fb367407a4368f16464921986bf02b9f81
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-02-20 18:07:08 +0100

    rename

--- Continuous delivery for Intune/HKCU/Install-HKCU.ps1
+++ Continuous delivery for Intune/HKCU/Install-HKCU.ps1
@@ -7,29 +7,29 @@ function Install-HKCU {
     $runDetectionRule = Invoke-Expression -Command $detection
     
     If (!($runDetectionRule -eq $true)) {
-        $TempRegFile = $env:TEMP + "\Temp.reg"
-        Remove-Item $TempRegFile -Force | Out-Null
-        Invoke-WebRequest -Uri $URL -OutFile $TempRegFile
-        $regfile = Get-Content $TempRegFile
+        $TempHKCUFile = $env:TEMP + "\Temp.reg"
+        Remove-Item $TempHKCUFile -Force | Out-Null
+        Invoke-WebRequest -Uri $URL -OutFile $TempHKCUFile
+        $regfile = Get-Content $TempHKCUFile
         $hives = Get-ChildItem -Path REGISTRY::HKEY_USERS | Select-Object -ExpandProperty Name
         foreach ($hive in $hives) {
             if (!($hive -like "*_Classes")) {
                 $newregfile = $regfile -replace "HKEY_CURRENT_USER",$hive
-                Set-Content -Path $TempRegFile -Value $newregfile
-                $Arguments = "/s $TempRegFile"
+                Set-Content -Path $TempHKCUFile -Value $newregfile
+                $Arguments = "/s $TempHKCUFile"
                 Start-Process "regedit.exe" -ArgumentList $Arguments -Wait
             }
         }
-        Remove-Item $TempRegFile -Force
+        Remove-Item $TempHKCUFile -Force
     }
 }
 
-$RegFileConf = $env:TEMP + "\RegFileConfig.JSON"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/HKCU/Continuous%20delivery%20for%20Intune/HKCU/config.json" -OutFile $RegFileConf
-$RegFiles = Get-Content $RegFileConf | ConvertFrom-Json
+$HKCUFileConf = $env:TEMP + "\HKCUFileConfig.JSON"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/HKCU/Continuous%20delivery%20for%20Intune/HKCU/config.json" -OutFile $HKCUFileConf
+$HKCUFiles = Get-Content $HKCUFileConf | ConvertFrom-Json
 
-foreach ($regfile in $RegFiles) {
-    Install-REGFile -URL $regfile.URL
+foreach ($hkcufile in $HKCUFiles) {
+    Install-HKCU -URL $hkcufile.URL
 }
 
-Remove-Item $RegFileConf -Force
+Remove-Item $HKCUFileConf -Force
