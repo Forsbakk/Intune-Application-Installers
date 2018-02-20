bb8f1ff54e38461bbb21d7e8a6d6c1c4097f2b64
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-02-20 16:51:39 +0100

    conf

--- Continuous delivery for Intune/HKCU/Create-Config.ps1
+++ Continuous delivery for Intune/HKCU/Create-Config.ps1
@@ -1,6 +1,6 @@
 $Downloads = @(
     @{
-        URL = ""
+        URL = "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/HKCU/Continuous%20delivery%20for%20Intune/HKCU/Test.reg"
     }
 )
 $Downloads | ConvertTo-Json -Compress | Out-File config.json
