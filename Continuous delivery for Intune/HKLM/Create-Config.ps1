8fea9fdc4fb415f9966cc5f709f93accd69fbd7b
subsequent <fanuelsen@mac.com>
2018-02-21 12:40:35 +0100

    Added TrustedSites

--- Continuous delivery for Intune/HKLM/Create-Config.ps1
+++ Continuous delivery for Intune/HKLM/Create-Config.ps1
@@ -2,6 +2,10 @@ $Downloads = @(
     @{
         URL = "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/master/Continuous%20delivery%20for%20Intune/HKLM/regfiles/DontDisplayLastUsername.reg"
         detection = "[bool]((Get-ItemPropertyValue -Path `"HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System`" -Name dontdisplaylastusername) -eq 0)"
+    },
+    @{
+        URL = "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/master/Continuous%20delivery%20for%20Intune/HKLM/regfiles/TrustedSites.reg"
+        detection = "[bool]((Get-ItemPropertyValue -Path `"HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMapKey`" -Name file://skole.i-sone.no) -eq 0)"
     }
 )
 $Downloads | ConvertTo-Json -Compress | Out-File config.json
