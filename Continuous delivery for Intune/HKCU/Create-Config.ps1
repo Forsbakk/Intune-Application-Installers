61a5fb3bd885a716013704b8cd469f0348c71cf1
subsequent <fanuelsen@mac.com>
2018-02-23 10:36:45 +0100

    Onedrive Silent Config

--- Continuous delivery for Intune/HKCU/Create-Config.ps1
+++ Continuous delivery for Intune/HKCU/Create-Config.ps1
@@ -10,6 +10,10 @@ $Downloads = @(
     @{
         URL = "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/master/Continuous%20delivery%20for%20Intune/HKCU/regfiles/TrustedSites.reg"
         detection = "[bool](Test-Path -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\kommune.no\adfs.horten)"
+    },
+    @{
+        URL = "https://raw.githubusercontent.com/Forsbakk/Intune-Application-Installers/master/Continuous%20delivery%20for%20Intune/HKCU/regfiles/EnableADAL.reg"
+        detection = "[bool]((Get-ItemPropertyValue -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\OneDrive -Name EnableADAL) -eq 1)"
     }
 )
 $Downloads | ConvertTo-Json -Compress | Out-File config.json
