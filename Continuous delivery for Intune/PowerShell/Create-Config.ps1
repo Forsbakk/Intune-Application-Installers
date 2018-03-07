baef6bf2a4afba8e9a1ddae7a0d3796f2e7d4237
subsequent <fanuelsen@mac.com>
2018-03-07 12:27:31 +0100

    Added remove vnc

--- Continuous delivery for Intune/PowerShell/Create-Config.ps1
+++ Continuous delivery for Intune/PowerShell/Create-Config.ps1
@@ -1,8 +1,8 @@
 $PowerShell = @(
     @{
-        Name = ""
-        Command = ""
-        Detection = ""
+        Name = "Remove VNC from Start Menu"
+        Command = "Remove-Item -Path `"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC`" -Recurse -Force"
+        Detection = "[bool](!(Test-Path -Path `"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC`"))"
     }
 )
 $PowerShell | ConvertTo-Json -Compress | Out-File config.json
