e5900dd5623af11f97be95245a638ce4e9b7b466
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-03-12 10:38:09 +0100

    Rename

--- Custom Scripts/Rename-Computer.ps1
+++ Custom Scripts/Rename-Computer.ps1
@@ -8,17 +8,23 @@
 #
 #Query to attain serial number
 #
-$NewName = Get-WmiObject -Class Win32_bios | Select -ExpandProperty SerialNumber
+$NewName = Get-WmiObject -Class Win32_bios | Select-Object -ExpandProperty SerialNumber
 
 #
 #Filters the serial number to 15 characters and removes spaces
 #
-$NewName = $NewName.Substring(0,15)-replace " "
+$Manufacturer = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty Manufacturer
+If ($Manufacturer -eq "Acer") {
+    $NewName = $NewName.Substring(10,12)-replace " "    
+}
+Else {
+    $NewName = $NewName.Substring(0,15)-replace " "
+}
 
 #
 #Query to attain the current computer name
 #
-$CurrentName = Get-WmiObject -Class Win32_ComputerSystem | Select -ExpandProperty Name
+$CurrentName = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty Name
 
 #
 #Rename the current computer name to the attained serial number
