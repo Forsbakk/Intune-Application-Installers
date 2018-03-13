d8f16dd2f940b582b8a0eb5e298d846fa36fe2ec
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-03-13 13:18:09 +0100

    init

---
+++ Choco/ChocoPkgs/Create-Config.ps1
@@ -0,0 +1,27 @@
+$ChocoPkgs = @(
+    @{
+        Name = "googlechrome"
+        Mode = "install"
+    },
+    @{
+        Name = "7zip"
+        Mode = "install"
+    },
+    @{
+        Name = "gimp"
+        Mode = "install"
+    },
+    @{
+        Name = "audacity"
+        Mode = "install"
+    },
+    @{
+        Name = "microsoft-teams"
+        Mode = "install"
+    },
+    @{
+        Name = "visualstudiocode"
+        Mode = "install"
+    }
+)
+$ChocoPkgs | ConvertTo-Json -Compress | Out-File ChocoConf.json
