ab838f8d3e04d7b741718f11ce4aa8183561424f
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-04-18 14:26:47 +0200

    script fix

--- Custom Scripts/Install-TS20DAFix.ps1
+++ Custom Scripts/Install-TS20DAFix.ps1
@@ -1,6 +1,8 @@
+$Driver = Get-WindowsDriver -Online -All | Where-Object {$_.OriginalFileName -like "*valleyview2system.inf"} | Select-Object -ExpandProperty Driver
+
 Expand-Archive -Path "C:\Windows\Temp\iaioi2ce.zip" -DestinationPath "C:\Windows\Temp\iaioi2ce"
 $PnPUtil = "pnputil.exe"
-$rmArg = "/delete-driver oem10.inf /uninstall /force"
+$rmArg = "/delete-driver $Driver /uninstall /force"
 $addArg = "/add-driver C:\Windows\Temp\iaioi2ce\iaioi2ce.inf /install"
 
 Start-Process -FilePath $PnPUtil -ArgumentList $rmArg -Wait
