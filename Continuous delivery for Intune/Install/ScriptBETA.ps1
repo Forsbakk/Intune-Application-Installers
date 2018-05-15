c377e59152997e5d31653675f5d99858b0899aa0
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-05-15 12:51:43 +0200

    added slmgr

--- Continuous delivery for Intune/Install/ScriptBETA.ps1
+++ Continuous delivery for Intune/Install/ScriptBETA.ps1
@@ -264,6 +264,45 @@ function Invoke-PowerShell {
     }
 }
 
+function Get-ActivationStatus {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [string]
+        $DNSHostName = $env:COMPUTERNAME
+    )
+    process {
+        try {
+            $wpa = Get-WmiObject SoftwareLicensingProduct -ComputerName $DNSHostName -Filter "ApplicationID = '55c92734-d682-4d71-983e-d6ec3f16059f'" -Property LicenseStatus -ErrorAction Stop
+        }
+        catch {
+            $status = New-Object ComponentModel.Win32Exception ($_.Exception.ErrorCode)
+            $wpa = $null    
+        }
+        $out = New-Object psobject -Property @{
+            ComputerName = $DNSHostName;
+            Status       = [string]::Empty;
+        }
+        if ($wpa) {
+            :outer foreach ($item in $wpa) {
+                switch ($item.LicenseStatus) {
+                    0 {$out.Status = "Unlicensed"}
+                    1 {$out.Status = "Licensed"; break outer}
+                    2 {$out.Status = "Out-Of-Box Grace Period"; break outer}
+                    3 {$out.Status = "Out-Of-Tolerance Grace Period"; break outer}
+                    4 {$out.Status = "Non-Genuine Grace Period"; break outer}
+                    5 {$out.Status = "Notification"; break outer}
+                    6 {$out.Status = "Extended Grace"; break outer}
+                    default {$out.Status = "Unknown value"}
+                }
+            }
+        }
+        else {$out.Status = $status.Message}
+        $out
+    }
+}
+
 
 $SerialNumber = Get-WmiObject -Class Win32_bios | Select-Object -ExpandProperty SerialNumber
 $Manufacturer = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty Manufacturer
@@ -282,6 +321,32 @@ Else {
 $CurrentName = $env:COMPUTERNAME
 If (!($CurrentName -eq $NewName)) {
     Rename-Computer -ComputerName $CurrentName -NewName $NewName
+}
+
+
+Write-Log -Value "Checking Windows 10 activation status" -Severity 1 -Component "slmgr"
+$Licensed = Get-ActivationStatus
+
+If ($Licensed -ne "Licensed") {
+    Write-Log -Value "Windows 10 was unlicensed; activating" -Severity 2 -Component "slmgr"
+    $ClientKey = "NW6C2-QMPVW-D7KKK-3GKT6-VCFB2"
+    $kmshost = "10.85.16.21"
+
+    $KMSservice = Get-WMIObject -query "select * from SoftwareLicensingService"
+    $KMSservice.InstallProductKey($ClientKey)
+    $KMSservice.SetKeyManagementServiceMachine($kmshost)
+    $KMSservice.RefreshLicenseStatus()
+
+    $Licensed = Get-ActivationStatus
+    If ($Licensed -ne "Licensed") {
+        Write-Log -Value "Windows 10 failed to license" -Severity 3 -Component "slmgr"
+    }
+    else {
+        Write-Log -Value "Windows 10 is licensed" -Severity 1 -Component "slmgr"
+    }
+}
+else {
+    Write-Log -Value "Windows 10 is licensed" -Severity 1 -Component "slmgr"
 }
 
 
