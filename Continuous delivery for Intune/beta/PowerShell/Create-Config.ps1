c141bfd694161eaf7a6a41be7d984b7ea01c44ff
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-05-22 10:31:58 +0200

    added teams uninstall to beta

--- Continuous delivery for Intune/beta/PowerShell/Create-Config.ps1
+++ Continuous delivery for Intune/beta/PowerShell/Create-Config.ps1
@@ -1,8 +1,13 @@
 $PowerShell = @(
     @{
-        Name = "Remove VNC from Start Menu"
-        Command = "Remove-Item -Path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC' -Recurse -Force"
+        Name      = "Remove VNC from Start Menu"
+        Command   = "Remove-Item -Path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC' -Recurse -Force"
         Detection = "[bool](!(Test-Path -Path `"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TightVNC`"))"
+    },
+    @{
+        Name = "Remove MS Teams DesktopEdt"
+        Command   = "Start-Process -FilePath `"C:\Users\Default\AppData\Local\Microsoft\Teams\Update.exe`" -ArgumentList `"--uninstall -s`""
+        Detection = "[bool]`$False"
     }
 )
 $PowerShell | ConvertTo-Json -Compress | Out-File config.json
